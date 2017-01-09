7a25d3599dcb2b8eef62abeba750f72f283e60ef
bhupeshchawda <bhupesh@apache.org>
2017-01-09 14:09:42 +0530

    SPOI-8514: Added content in readme. Changed app name. Added lisences.

--- tutorials/maprstreams/src/main/java/com/datatorrent/maprapp/Application.java
+++ tutorials/maprstreams/src/main/java/com/datatorrent/maprapp/Application.java
@@ -1,31 +1,47 @@
 /**
- * Put your copyright and license info here.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.datatorrent.maprapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.apex.malhar.kafka.KafkaSinglePortInputOperator;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
 import com.datatorrent.api.DAG;
+import com.datatorrent.api.DAG.Locality;
 import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
 import com.datatorrent.contrib.hbase.HBaseFieldInfo;
 import com.datatorrent.contrib.hbase.HBasePOJOPutOperator;
 import com.datatorrent.contrib.hbase.HBaseStore;
 import com.datatorrent.contrib.parser.JsonParser;
 import com.datatorrent.lib.util.FieldInfo;
 import com.datatorrent.lib.util.TableInfo;
-import org.apache.apex.malhar.kafka.KafkaSinglePortInputOperator;
-import org.apache.hadoop.conf.Configuration;
 
-import com.datatorrent.api.annotation.ApplicationAnnotation;
-import com.datatorrent.api.DAG.Locality;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@ApplicationAnnotation(name="MaprStreamsToMaprDB")
+@ApplicationAnnotation(name="MaprStreamsApp")
 public class Application implements StreamingApplication
 {
   private void createHBaseTable(HBaseStore store)
